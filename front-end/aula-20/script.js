009de6252312afad0df654fa163d360b97515e6e
Brrn91 <lucasbruno81@gmail.com>
2026-02-16 13:46:46 -0300

    array

--- front-end/aula-20/script.js
+++ front-end/aula-20/script.js
@@ -1,3 +1,7 @@
 const turma1 = ['Lucas', 'Bruno', 'Késia', 'Mayhélli'];
 const turma2 = ['Maria', 'João', 'José', 'Pedro'];
 
+console.log('Turma 1:' + turma1);
+console.log('Turma 2:' + turma2);
+
+console.log(turma1[0]);
