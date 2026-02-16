9f23901f77d55773a3b81e5a5fb15f6cb5143aaf
Brrn91 <lucasbruno81@gmail.com>
2026-02-16 17:19:43 -0300

    while, appenchild

--- front-end/aula-21/script.js
+++ front-end/aula-21/script.js
@@ -1,1 +1,14 @@
 const carros = ['Uno', 'Gol', 'Celta', 'Palio', 'Corsa'];
+const conteudo = document.getElementById('conteudo');
+
+let dados = '';
+let i = 0;
+
+while (i < 5) {
+  dados += '<p>'+carros[i]+'</p>';
+  i++;
+};
+
+const secao = document.createElement('div');
+secao.innerHTML = '<h2>Loop While</h2>' + dados;
+conteudo.appendChild(secao);
