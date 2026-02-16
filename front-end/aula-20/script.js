5715ffccd92b59e78fe3f5c5dda9d9eee94541f4
Brrn91 <lucasbruno81@gmail.com>
2026-02-16 14:00:01 -0300

    adicionando itens na array

--- front-end/aula-20/script.js
+++ front-end/aula-20/script.js
@@ -1,7 +1,16 @@
-const turma1 = ['Lucas', 'Bruno', 'Késia', 'Mayhélli'];
-const turma2 = ['Maria', 'João', 'José', 'Pedro'];
+// const turma1 = ['Lucas', 'Bruno', 'Késia', 'Mayhélli'];
+// const turma2 = ['Maria', 'João', 'José', 'Pedro'];
 
-console.log('Turma 1:' + turma1);
-console.log('Turma 2:' + turma2);
+// console.log('Turma 1:' + turma1);
+// console.log('Turma 2:' + turma2);
 
-console.log(turma1[0]);
+// console.log(turma1[0]);
+
+const listaDeCompras = ['Arroz', 'Feijão', 'Ovos'];
+console.log(listaDeCompras);
+listaDeCompras[3] = 'Farofa'; //Adicionei um elemento na array selecionando o índice 3.
+
+console.log(listaDeCompras);
+
+listaDeCompras.push('Maçã');
+console.log(listaDeCompras);
