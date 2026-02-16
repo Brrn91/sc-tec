06cb384dfed8c362342fe385a95172012f8cf5d6
Brrn91 <lucasbruno81@gmail.com>
2026-02-16 13:18:04 -0300

    calculadora simples

---
+++ front-end/aula-19-calculadora/script.js
@@ -0,0 +1,23 @@
+function efetuarOperacao() {
+  let num1 = parseFloat(document.getElementById('num1').value);
+  let num2 = parseFloat(document.getElementById('num2').value);
+  let operador = document.getElementById('operador').value;
+  let resultado;
+
+  switch (operador) {
+    case '+':
+      resultado = num1 + num2;
+      break;
+    case '-':
+      resultado = num1 - num2;
+      break;
+    case '*':
+      resultado = num1 * num2;
+      break;
+    case '/':
+      resultado = num1 / num2;
+      break;
+    default:
+      resultado = 'Operador inválido!';
+  }
+}
