0124aa151a234efeeef3a981b7e9868de33c9ac2
Brrn91 <lucasbruno81@gmail.com>
2026-02-17 16:16:27 -0300

    melhorias

--- front-end/aula-26-27/script.js
+++ front-end/aula-26-27/script.js
@@ -1,13 +1,20 @@
 const celulas = document.querySelectorAll('.celula');
 let vezDoX = true;
 
+document.getElementById('reiniciar').addEventListener('click', iniciarJogo);
+
 function iniciarJogo() {
+  vezDoX = true;
   celulas.forEach((celula) => {
     celula.textContent = '';
-    celula.addEventListener('click', function () {}, { once: true});
+    celula.removeEventListener('click', tratarClick);
+    celula.addEventListener('click', tratarClick, { once: true});
   });
 }
 
 function tratarClick(evento) {
   evento.target.textContent = vezDoX ? 'X' : 'O';
+  vezDoX = !vezDoX;
 };
+
+iniciarJogo();
