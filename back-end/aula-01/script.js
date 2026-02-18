6f2b843b2aa658912df579c95ca2a069b535053d
Brrn91 <lucasbruno81@gmail.com>
2026-02-18 08:00:20 -0300

    heranças

--- back-end/aula-01/script.js
+++ back-end/aula-01/script.js
@@ -1,18 +1,19 @@
 class Veiculo {
   constructor(fabricante, modelo, ano, tipo, qtdPortas) {
-    this.fabricante = fabricante; // Propriedade da Classe
-    this.modelo = modelo; // Propriedade da Classe
-    this.ano = ano; // Propriedade da Classe
-    this.tipo = tipo; // Propriedade da Classe
-    this.qtdPortas = qtdPortas; // Propriedade da Classe
-};
+    this.fabricante = fabricante;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.tipo = tipo;
+    this.qtdPortas = qtdPortas;
+  }
+}
 
 class Moto extends Veiculo {
-constructor(fabricante, modelo, ano) {
-
-}
+  constructor(fabricante, modelo, ano) {
+    // Chama o construtor da classe Veiculo
+    super(fabricante, modelo, ano, 'Moto', 0);
   }
-  
+
   mostrarDadosMoto() {
     console.log(`${this.fabricante} ${this.modelo}, ${this.ano}`);
   }
@@ -26,13 +27,11 @@ constructor(fabricante, modelo, ano) {
   }
 }
 
-class Carro {
+class Carro extends Veiculo {
   constructor(fabricante, modelo, ano, tipo, qtdPortas) {
-    this.fabricante = fabricante; // Propriedade da Classe
-    this.modelo = modelo; // Propriedade da Classe
-    this.ano = ano; // Propriedade da Classe
-
+    super(fabricante, modelo, ano, tipo, qtdPortas);
   }
+
   mostrarDadosCarro() {
     console.log(
       `${this.fabricante} ${this.modelo}, ${this.ano}, (${this.tipo}), ${this.qtdPortas} portas`
@@ -50,6 +49,6 @@ class Carro {
 
 const meuCarro = new Carro('Ford', 'Ka', '2008', 'Hatch', '4');
 const minhaMoto = new Moto('Honda', 'Africa Twin', '2026');
+
 meuCarro.mostrarDadosCarro();
 minhaMoto.mostrarDadosMoto();
-
