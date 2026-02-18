fea27def3f254f202525894956d4386e453c519a
Brrn91 <lucasbruno81@gmail.com>
2026-02-18 15:50:13 -0300

    dados finish

--- back-end/aula-03-04/script.js
+++ back-end/aula-03-04/script.js
@@ -8,13 +8,14 @@ setInterval(() => {
   console.log('Alerta!');
 }, 3000);
 */
+console.log('Inicio da execução.')
 
 function buscarDadosdoServidor() {
   //Código que busca dados do servidor
   return new Promise((resolve, reject) => {
     console.log('Buscando dados no servidor');
     setTimeout(() => {
-      let sucesso = Math.random > 0.5;
+      let sucesso = Math.random() > 0.5;
 
       if(sucesso) {
         resolve('Dados recebidos com sucesso!')
@@ -24,3 +25,11 @@ function buscarDadosdoServidor() {
     },2000);
 });
 }
+
+buscarDadosdoServidor().then((mensagem) => {
+  console.log(mensagem);
+}).catch((erro) => {
+  console.error(erro);
+});
+
+console.log('Final da execução!');
