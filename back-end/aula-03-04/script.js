63182c60579f0115fef0d8d51283f426135eec21
Brrn91 <lucasbruno81@gmail.com>
2026-02-18 15:35:38 -0300

    resolve, reject

--- back-end/aula-03-04/script.js
+++ back-end/aula-03-04/script.js
@@ -14,7 +14,13 @@ function buscarDadosdoServidor() {
   return new Promise((resolve, reject) => {
     console.log('Buscando dados no servidor');
     setTimeout(() => {
+      let sucesso = Math.random > 0.5;
 
+      if(sucesso) {
+        resolve('Dados recebidos com sucesso!')
+      } else {
+        reject('Falha ao buscar dados do servidor!');
+      }
     },2000);
 });
 }
