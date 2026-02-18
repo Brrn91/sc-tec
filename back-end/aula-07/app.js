a306635cbe069425bfed80e81469261c7c3a43b9
Brrn91 <lucasbruno81@gmail.com>
2026-02-18 20:48:41 -0300

    modulos

--- back-end/aula-07/app.js
+++ back-end/aula-07/app.js
@@ -1,7 +1,12 @@
-var pessoa = required('./commons/pessoa.js');
-var soma = required ('./commons/soma.js')
+var pessoa = required('./modulos-exemplo/commons/pessoa.js');
+var soma = required('./modulos-exemplo/commons/soma.js');
+var imposto = required('./modulos-exemplo/commons/imposto.js');
 
 lucas = pessoa();
 
 console.log(JSON.stringify(pessoa));
-console.log()
+console.log(soma(2, 2));
+
+console.log('O valor do produto com imposto: ' + imposto.adicionar(10));
+console.log('O valor do imposto' + imposto.valor);
+console.log('Taxa do imposto' + imposto.taxa);
