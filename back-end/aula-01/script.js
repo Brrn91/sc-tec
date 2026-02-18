d80affd3fea5277d3addfa09ba3a924be364b97b
Brrn91 <lucasbruno81@gmail.com>
2026-02-17 21:07:42 -0300

    propriedades

--- back-end/aula-01/script.js
+++ back-end/aula-01/script.js
@@ -1,12 +1,15 @@
 class Carro {
-  constructor(fabricante, marca, ano, tipo, qtdPortas) {
+  constructor(fabricante, modelo, ano, tipo, qtdPortas) {
     this.fabricante = fabricante; // Propriedade da Classe
-    this.marca = marca; // Propriedade da Classe
+    this.modelo = modelo; // Propriedade da Classe
     this.ano = ano; // Propriedade da Classe
     this.tipo = tipo; // Propriedade da Classe
     this.qtdPortas = qtdPortas; // Propriedade da Classe
   }
-  mostrarMarca() {
-    console.log(`A marca do carro é: ${this.marca}`);
+  mostrarDadosCarro() {
+    console.log(`${this.fabricante} ${this.modelo}, ${this.ano}, (${this.tipo}), ${this.qtdPortas} portas`);
   }
-};
+}
+
+const meuCarro = new Carro("Ford", 'Ka', '2008', 'Hatch', '4');
+meuCarro.mostrarDadosCarro();
