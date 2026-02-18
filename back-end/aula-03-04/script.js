2eb41544138fba453b57003a68661ae5136c4360
Brrn91 <lucasbruno81@gmail.com>
2026-02-18 10:28:31 -0300

    buscarDadosDoServidor

--- back-end/aula-03-04/script.js
+++ back-end/aula-03-04/script.js
@@ -2,8 +2,19 @@
 setTimeout(() => {
   console.log('Alerta!');
 }, 3000);
-*/
+
 
 setInterval(() => {
   console.log('Alerta!');
 }, 3000);
+*/
+
+function buscarDadosdoServidor() {
+  //Código que busca dados do servidor
+  return new Promise((resolve, reject) => {
+    console.log('Buscando dados no servidor');
+    setTimeout(() => {
+
+    },2000);
+});
+}
