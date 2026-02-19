62d627a95370692ddf5e0ca02275704eff79c959
Brrn91 <lucasbruno81@gmail.com>
2026-02-19 11:01:22 -0300

    views

--- back-end/aula-08-09-10/app.ts
+++ back-end/aula-08-09-10/app.ts
@@ -3,6 +3,8 @@ import clientRouter from './router/client.js'
 const app = express();
 
 app.use(clientRouter);
+app.set('view engine', 'pug');
+app.set('views', './views')
 
 app.listen(3000, () => {
   console.log('Servidor criado...')
