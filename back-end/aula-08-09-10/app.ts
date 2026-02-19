e6f95c36917b6d7c786afb5ed1770d97c283ffbb
Brrn91 <lucasbruno81@gmail.com>
2026-02-19 14:54:27 -0300

    organizando

--- back-end/aula-08-09-10/app.ts
+++ back-end/aula-08-09-10/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import clientRouter from './router/client.js'
+import db from './db.js'
 
 const app = express();
 
 app.use(clientRouter);
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
 
-app.listen(process.env.PORT, () => {
-  console.log('Servidor criado...')
-});
+db.sync().then(() => {
+  console.log('Conectado com o banco: ' + process.env.DB_NAME);
+}).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log('Servidor criado...')
+  });
+})
--- back-end/aula-08-09-10/db.ts
+++ back-end/aula-08-09-10/db.ts
@@ -1,1 +1,13 @@
 import { Sequelize } from "sequelize";
+
+const dbName = process.env.DB_NAME ?? "";
+const dbUser = process.env.DB_USER ?? "";
+const dbPass = process.env.DB_PASS ?? "";
+const dbHost = process.env.DB_HOST ?? "localhost";
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  dialect: "mysql",
+  host: dbHost
+});
+
+export default sequelize;
