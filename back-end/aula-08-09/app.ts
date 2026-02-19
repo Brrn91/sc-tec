4a3c76a2207f0bfd1cfe88b6226f4561c851c4bd
Brrn91 <lucasbruno81@gmail.com>
2026-02-19 10:44:49 -0300

    rotas

--- back-end/aula-08-09/app.ts
+++ back-end/aula-08-09/app.ts
@@ -1,6 +1,22 @@
 import express from "express";
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Rota inicial!</h1>')
+});
+
+app.get('/sobrenos', (req, res) => {
+  res.send('Rota falando sobre a empresa');
+});
+
+app.get('/trabalheconosco', (req, res) => {
+  res.send('Opções de carreira');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('(48) 99999-9999');
+});
+
 app.listen(3000, () => {
   console.log('Servidor criado...')
 });
