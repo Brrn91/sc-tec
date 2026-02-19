78bd3b67ce5fc182e6c8723a295965065ce0561d
Brrn91 <lucasbruno81@gmail.com>
2026-02-19 10:48:49 -0300

    import

--- back-end/aula-08-09/app.ts
+++ back-end/aula-08-09/app.ts
@@ -1,21 +1,8 @@
 import express from "express";
+import clientRouter from '../aula-08-09/router/client.js'
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Rota inicial!</h1>')
-});
-
-app.get('/sobrenos', (req, res) => {
-  res.send('Rota falando sobre a empresa');
-});
-
-app.get('/trabalheconosco', (req, res) => {
-  res.send('Opções de carreira');
-});
-
-app.get('/contato', (req, res) => {
-  res.send('(48) 99999-9999');
-});
+app.use(clientRouter);
 
 app.listen(3000, () => {
   console.log('Servidor criado...')
